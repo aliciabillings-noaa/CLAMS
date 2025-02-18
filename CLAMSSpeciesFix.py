cb31e12f4c4f26fda6ffd3990d36a97c127dee49
Melina.Shak <1549476674113305@mil>
2025-02-18 14:21:14 -0800

    fixes to resolve crash issues

--- CLAMSSpeciesFix.py
+++ CLAMSSpeciesFix.py
@@ -17,8 +17,7 @@ class CLAMSSpeciesFix(QDialog, ui_CLAMSSpeciesFix.Ui_clamsSpeciesFix):
         self.setupUi(self)
         #self.setAttribute(Qt.WA_DeleteOnClose)
         self.db=parent.db
-        if not self.db.isOpen():
-            self.db.open()
+        self.db.dbOpen()
         self.workStation=parent.workStation
         self.survey=parent.survey
         self.ship=parent.ship
@@ -27,7 +26,7 @@ class CLAMSSpeciesFix(QDialog, ui_CLAMSSpeciesFix.Ui_clamsSpeciesFix):
         self.settings=parent.settings
         self.errorSounds=parent.errorSounds
         self.errorIcons=parent.errorIcons
-        self.backLogger=parent.backLogger
+      #  self.backLogger=parent.backLogger
         #setup reoccuring dlgs
         self.numDialog = numpad.NumPad(self)
         self.message=messagedlg.MessageDlg(self)
@@ -50,7 +49,7 @@ class CLAMSSpeciesFix(QDialog, ui_CLAMSSpeciesFix.Ui_clamsSpeciesFix):
                 self.survey+" AND samples.event_id="+self.activeHaul+" AND samples.partition='"+
                 self.activePartition+"' AND baskets.basket_type='Measure' AND samples.species_code<>0 " +
                 "GROUP BY species.common_name, samples.species_code, species.scientific_name, " +
-                "samples.sample_id,  samples.subcategory",  self.db)
+                "samples.sample_id,  samples.subcategory")
         query = self.db.dbQuery(sql)
 
         self.sampleDict={}
@@ -71,22 +70,34 @@ class CLAMSSpeciesFix(QDialog, ui_CLAMSSpeciesFix.Ui_clamsSpeciesFix):
         font = QFont('helvetica', 14, -1, False)
         self.measureView.setFont(font)
         self.measureModel = QtSql.QSqlQueryModel()
-        self.measureView.setSelectionMode(QAbstractItemView.SingleSelection)
-        self.measureView.setSelectionBehavior(QAbstractItemView.SelectRows)
-        self.measureView.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.measureView.setSelectionMode(QAbstractItemView.SelectionMode.SingleSelection)
+        self.measureView.setSelectionBehavior(QAbstractItemView.SelectionBehavior.SelectRows)
+        self.measureView.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
         self.measureView.setModel(self.measureModel)
         self.selModel = QItemSelectionModel(self.measureModel, self.measureView)
         self.measureView.setSelectionModel(self.selModel)
-        self.measureView.horizontalHeader().setResizeMode(QHeaderView.ResizeToContents)
+        self.measureView.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.ResizeToContents)
 
         self.measureView.show()
 
         # set up window position
-        screen=QDesktopWidget().screenGeometry()
-        window=self.geometry()
-        self.setGeometry((screen.width()-window.width())/2,parent.windowAnchor[0]+(parent.windowAnchor[1]-window.height()), window.width(), window.height())
-        self.setMinimumSize(window.width(), window.height())
-        self.setMaximumSize(window.width(), window.height())
+       # screen=QDesktopWidget().screenGeometry()
+       # window=self.geometry()
+       # self.setGeometry((screen.width()-window.width())/2,parent.windowAnchor[0]+(parent.windowAnchor[1]-window.height()), window.width(), window.height())
+       # self.setMinimumSize(window.width(), window.height())
+       # self.setMaximumSize(window.width(), window.height())
+
+        #  restore the application state
+        self.appSettings = QSettings('CLAMS', 'SpeciesFix')
+        size = self.appSettings.value('winsize', QSize(1000, 650))
+        position = self.appSettings.value('winposition', QPoint(10, 10))
+
+        #  check the current position and size to make sure the app is on the screen
+        position, size = self.checkWindowLocation(position, size)
+
+        #  now move and resize the window
+        self.move(position)
+        self.resize(size)
 
         # general slots
         self.doneBtn.clicked.connect(self.goExit)
@@ -210,8 +221,8 @@ class CLAMSSpeciesFix(QDialog, ui_CLAMSSpeciesFix.Ui_clamsSpeciesFix):
                                 " ship=" + self.ship +" AND survey=" + self.survey + " AND haul=" + self.activeHaul +
                                 " AND partition='" + self.activePartition + "' "+self.filterString+" GROUP BY scientist ORDER BY scientist")
             query = self.db.dbQuery(sql)
-            while query.next():
-                self.scientistBox.addItem(query.value(0).toString())
+            for value in query:
+                self.scientistBox.addItem(value[0])
             self.scientistBox.setCurrentIndex(-1)
 
         if self.workstationBox.currentIndex() ==-1:
@@ -220,8 +231,8 @@ class CLAMSSpeciesFix(QDialog, ui_CLAMSSpeciesFix.Ui_clamsSpeciesFix):
                                 " ship=" + self.ship +" AND survey=" + self.survey + " AND haul=" + self.activeHaul +
                                 " AND partition='" + self.activePartition + "' "+self.filterString+" GROUP BY workstation_ID ORDER BY workstation_ID")
             query = self.db.dbQuery(sql)
-            while query.next():
-                self.workstationBox.addItem(query.value(0).toString())
+            for value in query:
+                self.workstationBox.addItem(value[0])
             self.workstationBox.setCurrentIndex(-1)
 
         if self.speciesBox.currentIndex() ==-1:
@@ -230,14 +241,14 @@ class CLAMSSpeciesFix(QDialog, ui_CLAMSSpeciesFix.Ui_clamsSpeciesFix):
                                 " ship=" + self.ship + " AND survey=" + self.survey + " AND haul=" + self.activeHaul +
                                 " AND partition='" + self.activePartition + "' "+self.filterString+" GROUP BY species_code, common_name, subcategory, sample_id ORDER BY species_code")
             query = self.db.dbQuery(sql)
-            while query.next():
-                if query.value(2).toString() != 'None':
-                    species_tag=query.value(1).toString()+'-'+query.value(2).toString()
+            for value in query:
+                if value[2] != 'None':
+                    species_tag=value[1]+'-'+value[2]
                 else:
-                    species_tag=query.value(1).toString()
+                    species_tag=value[1]
 
                 self.speciesBox.addItem(species_tag)
-                self.oldSampleDict.update({species_tag:query.value(3).toString()})
+                self.oldSampleDict.update({species_tag:value[3]})
             self.speciesBox.setCurrentIndex(-1)
 
     def changeSpeciesAssignment(self):
@@ -379,7 +390,7 @@ class CLAMSSpeciesFix(QDialog, ui_CLAMSSpeciesFix.Ui_clamsSpeciesFix):
                             " ship=" + self.ship +" AND survey=" + self.survey + " AND haul=" + self.activeHaul +
                             " AND partition='" + self.activePartition + "' "+self.filterString+" ORDER BY SPECIMEN_ID")
 
-        self.measureModel.reset()
+        self.measureModel.beginResetModel()
         self.measureView.scrollToBottom()
 
 
@@ -388,7 +399,76 @@ class CLAMSSpeciesFix(QDialog, ui_CLAMSSpeciesFix.Ui_clamsSpeciesFix):
         self.close()
 
     def closeEvent(self, event):
+        #  store the application size and position
+        self.appSettings.setValue('winposition', self.pos())
+        self.appSettings.setValue('winsize', self.size())
         event.accept()
 
+    def checkWindowLocation(self, position, size, padding=[5, 25]):
+        '''
+        checkWindowLocation accepts a window position (QPoint) and size (QSize)
+        and returns a potentially new position and size if the window is currently
+        positioned off the screen.
 
+        This function uses QScreen.availableVirtualGeometry() which returns the full
+        available desktop space *not* including taskbar. For all single and "typical"
+        multi-monitor setups this should work reasonably well. But for multi-monitor
+        setups where the monitors may be different resolutions, have different
+        orientations or different scaling factors, the app may still fall partially
+        or totally offscreen. A more thorough check gets complicated, so hopefully
+        those cases are very rare.
+
+        If the user is holding the <shift> key while this method is run, the
+        application will be forced to the primary monitor.
+        '''
+
+        #  create a QRect that represents the app window
+        appRect = QRect(position, size)
+
+        #  check for the shift key which we use to force a move to the primary screem
+        resetPosition = QGuiApplication.queryKeyboardModifiers() == Qt.KeyboardModifier.ShiftModifier
+        if resetPosition:
+            position = QPoint(padding[0], padding[0])
+
+        #  get a reference to the primary system screen - If the app is off the screen, we
+        #  will restore it to the primary screen
+        primaryScreen = QGuiApplication.primaryScreen()
+
+        #  assume the new and old positions are the same
+        newPosition = position
+        newSize = size
+
+        #  Get the desktop geometry. We'll use availableVirtualGeometry to get the full
+        #  desktop rect but note that if the monitors are different resolutions or have
+        #  different scaling, some parts of this rect can still be offscreen.
+        screenGeometry = primaryScreen.availableVirtualGeometry()
+
+        #  if the app is partially or totally off screen or we're force resetting
+        if resetPosition or not screenGeometry.contains(appRect):
+
+            #  check if the upper left corner of the window is off the left side of the screen
+            if position.x() < screenGeometry.x():
+                newPosition.setX(screenGeometry.x() + padding[0])
+            #  check if the upper right is off the right side of the screen
+            if position.x() + size.width() >= screenGeometry.width():
+                p = screenGeometry.width() - size.width() - padding[0]
+                if p < padding[0]:
+                    p = padding[0]
+                newPosition.setX(p)
+            #  check if the top of the window is off the top/bottom of the screen
+            if position.y() < screenGeometry.y():
+                newPosition.setY(screenGeometry.y() + padding[0])
+            if position.y() + size.height() >= screenGeometry.height():
+                p = screenGeometry.height() - size.height() - padding[1]
+                if p < padding[0]:
+                    p = padding[0]
+                newPosition.setY(p)
+
+            #  now make sure the lower right (resize handle) is on the screen
+            if (newPosition.x() + newSize.width()) > screenGeometry.width():
+                newSize.setWidth(screenGeometry.width() - newPosition.x() - padding[0])
+            if (newPosition.y() + newSize.height()) > screenGeometry.height():
+                newSize.setHeight(screenGeometry.height() - newPosition.y() - padding[1])
+
+        return [newPosition, newSize]
 
