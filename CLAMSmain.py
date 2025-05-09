23aec3640306174caf9b906a94fec1aed408879d
Rick Towler <32103294+rhtowler@users.noreply.github.com>
2025-05-09 14:01:38 -0700

    Minor change to homescreen image handling
    also, removed a bunch of old background images.

--- CLAMSmain.py
+++ CLAMSmain.py
@@ -142,7 +142,7 @@ class CLAMSMain(QMainWindow, ui_CLAMSMain.Ui_clamsMain):
         try:
             #  the background image is based on the app version number
             imFile = (self.settings['ImageDir'] + '/backgrounds/' +
-                    os.sep + homeScreenImg + ".png")
+                    os.sep + settings['HomeScreenImg'])
             imFile = os.path.normpath(imFile)
 
             #  the image URL must use forward slashes
@@ -692,7 +692,6 @@ if __name__ == "__main__":
     dataSource = initSettings.value('ODBC_Data_Source', '')
     user = initSettings.value('User', '')
     password = initSettings.value('Password', '')
-    homeScreenImg = initSettings.value('HomeScreenImg', '')
 
     #  extract the application paths and settings
     settings = {}
@@ -702,6 +701,7 @@ if __name__ == "__main__":
     settings['IconDir'] = initSettings.value('IconDir', './icons')
     settings['Database'] = initSettings.value('Database', 'Oracle')
     settings['GUI-ScrollBar-Width'] = initSettings.value('GUI-ScrollBar-Width', 40)
+    settings['HomeScreenImg'] = initSettings.value('HomeScreenImg', 'default.jpg')
 
     #  create an instance of QApplication
     app = QApplication(sys.argv)
