8bb301c1c949d66dca451ba0e343ad8e8cd34303
Melina.Shak <1549476674113305@mil>
2026-03-18 13:44:28 -0700

    deleting code that was previously commented out to resolve unsorted scale issues

--- CPS/unsortedCatch.py
+++ CPS/unsortedCatch.py
@@ -279,18 +279,6 @@ class unsortedCatch(QDialog, ui_CPSUnsortedCatch.Ui_CPSUnsortedCatch):
         if 'basket_weight' not in self.deviceData[device_name]['measurements']['catch']:
             return
 
-        # check if a species is selected
-        #if self.activeSpcName == None:
-        #    self.message.setMessage(self.errorIcons[2],self.errorSounds[2], self.firstName +
-        #            ", please select a species.",'info')
-        #    self.message.exec()
-        #    return
-
-        #  check if the current sample type is "Present" and ignore input if so.
-        #  we don't allow baskets to be assigned to Present samples.
-       # if self.activeSampleType in ['Present', None]:
-       #     return
-
         #  ensure that the value is numeric - noise on the data lines, poor connections,
         #  or bad power can result in garbled data.
         try:
