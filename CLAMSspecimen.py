a10f3c399011d624ec2be91264eabbc75f72fcb1
Brittany Schwartzkopf <brittany.schwartzkopf@noaa.gov>
2026-05-19 13:58:43 -0700

    missed a spot to add stomach barcode

--- CLAMSspecimen.py
+++ CLAMSspecimen.py
@@ -813,7 +813,7 @@ class CLAMSSpecimen(QDialog, ui_CLAMSSpecimen.Ui_clamsSpecimen):
         else:
             #  check if this is a manually entered value or from a device
             if self.manualFlag:
-                if (self.measureType[i] == 'alpha_barcode'):
+                if (self.measureType[i] == 'alpha_barcode' or self.measureType[i] == 'stomach_barcode'):
                     #  this value is entered manually - display the swfsc number pad
                     keyDialog = swfscbarcodenumpad.SWFSCBarcodeNumpad(self.values[i], self)
                     keyDialog.msgLabel.setText("Enter " + self.measureType[i])
